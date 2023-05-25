f1108eca782a1c764d067163ca75270d02ba8c51
Arthur <lakiza@gmail.com>
2023-05-26 02:36:15 +0300

    Добавил кэширование в редис

--- assistant/src/services/content/content.py
+++ assistant/src/services/content/content.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 from functools import lru_cache
 from http import HTTPStatus
 
+from fastapi_cache.decorator import cache
+
 from .abstract import AbstractContent
 from core.config import settings
 from models.content import Film
@@ -25,7 +27,7 @@ class Content(AbstractContent):
             return None
 
         url = settings.content_film_get + films[0].get("id")
-        http_result = await self._fetch(url=url)
+        http_result = await self.fetch_data(url=url)
 
         if http_result and http_result.status == HTTPStatus.OK:
             return Film(**http_result.body)
@@ -35,19 +37,26 @@ class Content(AbstractContent):
     async def search_film(self, name: str) -> list[dict]:
         films = []
 
-        http_result = await self._fetch(
-            url=settings.content_films_search,
-            params={"query": name}
-        )
+        http_result = await self.fetch_data(url=settings.content_films_search, params={"query": name})
         if http_result and http_result.status == HTTPStatus.OK:
             films = http_result.body.get("results", [])
 
         return films
 
-    @staticmethod
-    async def _fetch(url: str, params: dict | None = None,
+    async def fetch_data(self, url: str, params: dict | None = None,
                      json: dict | None = None, method: str = 'GET') -> HTTPResponse | None:
         """Отправка HTTP запроса."""
+        dict_data = await self._fetch(url, params, json, method)
+        if dict_data:
+            return HTTPResponse(**dict_data)
+
+        return None
+
+    @staticmethod
+    @cache()
+    async def _fetch(url: str, params: dict | None = None,
+                     json: dict | None = None, method: str = 'GET') -> dict | None:
+        """Отправка HTTP запроса с кэшированием результат."""
         params = params or {}
         json = json or {}
 
@@ -57,12 +66,14 @@ class Content(AbstractContent):
                     logger.info("HTTP {0} request: url={1} params={2} json={3}".format(
                         method, url, params, json
                     ))
-                    return HTTPResponse(
-                        body=await response.json(),
-                        status=response.status,
-                    )
+                    return {
+                        "body": await response.json(),
+                        "status": response.status,
+                    }
             except aiohttp.ClientError as e:
                 logger.error("Async API error: {0}".format(e))
+
+        return None
 
 
 @lru_cache
